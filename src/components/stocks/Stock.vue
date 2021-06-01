324893f425e0dabf135a013db62aa1c2dc85566e
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-06-01 13:55:44 -0300

    Stocks layout created

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Action</h1>
+  <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+    <v-card class="green darken-3 white--text">
+      <v-card-title class="headline">
+        <strong>NAME <small>(Price: PRICE)</small></strong>
+      </v-card-title>
+    </v-card>
+    <v-card>
+      <v-container fill-height>
+        <v-text-field label="Amount" type="number" />
+        <v-btn class="green darken-3 white--text">
+          Buy
+        </v-btn>
+      </v-container>
+    </v-card>
+  </v-flex>
 </template>
 
 <script>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Actions</h1>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" />
+  </v-layout>
 </template>
 
 <script>
+import Stock from './Stock'
 export default {
-  
+  components: {Stock},
+  data(){
+    return{
+      stocks: [
+        {id: 1, name: 'BTS Card', price: 120},
+        {id: 2, name: 'BlackPink Card', price: 80},
+        {id: 3, name: 'TxT Card', price: 100},
+        {id: 4, name: 'ITZY Card', price: 95},
+      ]
+    }
+  }
 }
 </script>
 
