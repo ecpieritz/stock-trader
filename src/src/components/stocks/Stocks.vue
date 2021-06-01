72de6de7fbf674753c15910afbc995e4972b2095
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-06-01 14:12:05 -0300

    Added functionality in the Buy button

---
+++ src/src/components/stocks/Stocks.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout row wrap>
+    <Stock v-for="stock in stocks" :key="stock.id" :stock="stock" />
+  </v-layout>
+</template>
+
+<script>
+import Stock from './Stock'
+export default {
+  components: {Stock},
+  data(){
+    return{
+      stocks: [
+        {id: 1, name: 'BTS Card', price: 120},
+        {id: 2, name: 'BlackPink Card', price: 80},
+        {id: 3, name: 'TxT Card', price: 100},
+        {id: 4, name: 'ITZY Card', price: 95},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
