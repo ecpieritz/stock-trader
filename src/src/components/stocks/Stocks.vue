97312e36eb2aad9e63822ce7ba226909772851c6
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-06-06 09:17:41 -0300

    Data refactored and externalized

--- src/src/components/stocks/Stocks.vue
+++ src/src/components/stocks/Stocks.vue
@@ -8,14 +8,9 @@
 import Stock from './Stock'
 export default {
   components: {Stock},
-  data(){
-    return{
-      stocks: [
-        {id: 1, name: 'BTS Card', price: 120},
-        {id: 2, name: 'BlackPink Card', price: 80},
-        {id: 3, name: 'TxT Card', price: 100},
-        {id: 4, name: 'ITZY Card', price: 95},
-      ]
+  computed:{
+    stocks(){
+      return this.$store.getters.stocks
     }
   }
 }
---
+++ src/src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  {id: 1, name: 'BTS Card', price: 120},
+  {id: 2, name: 'BlackPink Card', price: 80},
+  {id: 3, name: 'TxT Card', price: 100},
+  {id: 4, name: 'ITZY Card', price: 95},
+]
