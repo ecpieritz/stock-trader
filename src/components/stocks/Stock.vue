0a84357d61fdc036b610270143c00f7788248b0b
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-06-06 08:57:01 -0300

    Added vuex to the project

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,13 +2,13 @@
   <v-flex class="pr-3 pb-3" xs12 md6 lg4>
     <v-card class="green darken-3 white--text">
       <v-card-title class="headline">
-        <strong>NAME <small>(Price: PRICE)</small></strong>
+        <strong>{{stock.name}} <small>(Price: {{stock.price}})</small></strong>
       </v-card-title>
     </v-card>
     <v-card>
       <v-container fill-height>
-        <v-text-field label="Amount" type="number" />
-        <v-btn class="green darken-3 white--text">
+        <v-text-field label="Amount" type="number" v-model.number="quantity" min="0" />
+        <v-btn class="green darken-3 white--text" @click="buyStock" :disabled="quantity <= 0 || !Number.isInteger(quantity)" >
           Buy
         </v-btn>
       </v-container>
@@ -18,7 +18,23 @@
 
 <script>
 export default {
-  
+  props: ['stock'],
+  data(){
+    return{
+      quantity: 0
+    }
+  },
+  methods:{
+    buyStock(){
+      const order = {
+        stockId: this.stock.id,
+        stockPrice: this.stock.price,
+        quantity: this.quantity
+
+      }
+      this.quantity = 0
+    }
+  }
 }
 </script>
 
